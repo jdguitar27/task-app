9dc004e0802b0aa1ceb5c3eeab212303aed13619
Jose Duran <jose27duran08@gmail.com>
2019-02-21 02:16:14 -0400

    creacion del proyecto

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hola");
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+// Settings
+
+// Coloca el puerto a utilizar
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+
+//Morgan genera información acerca de las peticiones 
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Routes
+app.use('/api/tasks', require('./routes/task.routes'));
+
+app.listen(app.get('port'), () => {
+    console.log(`Servidor corriendo en puerto ${app.get('port')}`);
+});
